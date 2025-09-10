50f17f860a029a91f7e77dac6037e69be125ad80
Chuseok22 <bjh59629@naver.com>
2025-09-10 14:49:02 +0900

    로그인화면 개발 : feat : 로그인 페이지 init https://github.com/TEAM-ROMROM/RomRom-Admin/issues/1

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/common/login-form";
+
+export default function Login() {
+  return (
+      <div className="flex justify-center items-center min-h-screen">
+        <LoginForm />
+      </div>
+  )
+};
