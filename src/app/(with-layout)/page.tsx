b7489cb18f62c60eca38e218c8966eec575ce485
Chuseok22 <bjh59629@naver.com>
2025-09-17 10:14:24 +0900

    로그아웃 버튼 클릭 시 확인 Alert 추가 : feat : 로그아웃 dialog 컴포넌트 추가 및 sidebar 로그아웃 버튼 클릭시 dialog 출력 https://github.com/TEAM-ROMROM/RomRom-Admin/issues/16

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,10 +1,12 @@
 import Chart from "@/components/common/chart";
+import LogoutAlertDialog from "@/app/(with-layout)/_shared/components/logout-alert-dialog";
 
 export default function Home() {
   return (
       <div>
         <Chart />
         <Chart />
+        <LogoutAlertDialog/>
       </div>
   );
 }
