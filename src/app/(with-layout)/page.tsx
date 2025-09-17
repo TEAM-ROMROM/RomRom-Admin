be05ceb99905e65790af6ec58acbd29a11b31f1c
Chuseok22 <bjh59629@naver.com>
2025-09-17 10:16:54 +0900

    로그아웃 버튼 클릭 시 확인 Alert 추가 : feat : 빌드 오류 해결 https://github.com/TEAM-ROMROM/RomRom-Admin/issues/16

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,12 +1,10 @@
 import Chart from "@/components/common/chart";
-import LogoutAlertDialog from "@/app/(with-layout)/_shared/components/logout-alert-dialog";
 
 export default function Home() {
   return (
       <div>
         <Chart />
         <Chart />
-        <LogoutAlertDialog/>
       </div>
   );
 }
